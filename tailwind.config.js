f28bc0be001bb4e0c7644c3c232923b09126a492
njteh-88 <n.nalchajian8@gmail.com>
2024-06-27 23:08:12 +0400

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './index.css',
+    './tailwind.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
